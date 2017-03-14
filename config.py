2e98955914b5c91a78b4811677eae8ceb1766453
Kiritee <kiritee.amruth@innovaccer.com>
2017-03-14 10:20:26 +0530

    import glo. vars

---
+++ config.py
@@ -0,0 +1,11 @@
+from pymongo import MongoClient
+
+add = "localhost"
+port = 27017
+db = "khojcancion"
+collection = "lyrics"
+mongoclientObj = MongoClient(add,port)[db][collection]
+
+base_url = "http://www.allthelyrics.com"
+index_url = "http://www.allthelyrics.com/index"
+alphabets = list("abcdefghiklmnopqrstuvwxyz0123456789")
