c36ebfde2e202c8508a809b6aabfae76f4342d5b
Kiritee <kiritee.amruth@innovaccer.com>
2017-03-14 10:19:49 +0530

    Added db storage and lyric extraction crawler

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 # -*- coding: utf-8 -*-
 from bs4 import BeautifulSoup
 import requests, urllib2, re
+from pymongo import MongoClient
+from pymongo.errors import DuplicateKeyError
 
-'''
-	For each alphabet
-	For each page of the alphabet
-	For each artist in that alphabet
-	For each song of that artist
-	-- Get lyrics
-'''
+from utilities import *
+from config import *
 
 def main(base_url, index_url, alphabets):
 	for letter in alphabets:
@@ -19,15 +16,29 @@ def main(base_url, index_url, alphabets):
 			page_url_modified = leaf1_url+"/"+letter+str(pageNumber)+".htm"
 			refined_links = refineLinks(getLinks(page_url_modified), "/lyrics", "title", False)
 			for each_link in refined_links:
-				getAllSongs(base_url+"/"+each_link, each_link, "title")
-			exit()
+				song_links = list(set(getAllSongs(base_url+"/"+each_link, each_link, "title")))
+				for each_song in song_links:
+					song_url = base_url+"/"+each_song
+					(lyrics, song_name) = getSongLyrics(song_url)
+					try:
+						makeDocumentAndPush(each_link.split('/')[-1], song_name, song_url, lyrics)
+					except DuplicateKeyError as e:
+						print e
+						pass
 
-def getHtmlResponse(url):
-	return urllib2.urlopen(url).read()
+def makeDocumentAndPush(artist, song_name, lyric_url, lyrics):
+	doc = {
+			'_id' : artist+"_"+song_name,
+			'artist_name': artist,
+			'song' : song_name,
+			'lyrics' : lyrics,
+			'lyric link' :lyric_url
+		}
+	mongoclientObj.insert(doc)
 
-def getLinks(url):
-	soup = BeautifulSoup(getHtmlResponse(url),'html.parser')
-	return soup.find_all(href=True)
+def getSongLyrics(song_url):
+	soup = BeautifulSoup(getHtmlResponse(song_url),'html.parser')
+	return soup.find_all('div',{"class":"content-text-inner"})[0].text.replace("\n"," "), soup.find_all('h1',{"class":"page-title"})[0].text.lower().replace(" lyrics","")
 
 def refineLinks(list_of_links, baseword_href, baseword_title, node_branch):
 	out_links = []
@@ -55,15 +66,11 @@ def maxPageForaAlphabet(list_of_links):
 	return max(list([int(re.findall(r'/index/a/a(.*?).htm',link,re.IGNORECASE)[0]) for link in list_of_links]))
 
 def getPages(element_tag_links):
-	allPagesForLetter = getNoOfPages(element_tag_links,"Go to page")
-	return maxPageForaAlphabet(allPagesForLetter)
+	pages = getNoOfPages(element_tag_links,"Go to page")
+	return maxPageForaAlphabet(pages)
 
 def getAllSongs(page_url, baseword_href, baseword_title):
-	print refineLinks(getLinks(page_url), baseword_href, baseword_title, node_branch = True)
-	exit()
+	return refineLinks(getLinks(page_url), baseword_href, baseword_title, node_branch = True)
 
 if __name__ == '__main__':
-	base_url = "http://www.allthelyrics.com"
-	index_url = "http://www.allthelyrics.com/index"
-	alphabets = list("abcdefghiklmnopqrstuvwxyz0123456789")
 	main(base_url, index_url, alphabets)
